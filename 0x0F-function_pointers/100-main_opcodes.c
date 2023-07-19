9c33810a22808f5a453965af8933928ea42adb12
Gracedchild001 <isaac.uched@gmail.com>
2023-07-19 12:51:47 -0700

    0x0F. C - Function pointers task update v1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
 *main - outputs opcodes
-*@argc: arg number
-*@argv: arg array
+*@argc: arguments numbers
+*@argv: arguments array
 *Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
@@ -15,12 +17,14 @@ printf("Error\n");
 exit(1);
 }
 bytes = atoi(argv[1]);
+
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
 arr = (char *)main;
+
 for (i = 0; i < bytes; i++)
 {
 if (i == bytes - 1)
