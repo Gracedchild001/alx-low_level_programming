68a7e3353c22e4ab6a3073fe8cf256199c6bf029
Gracedchild001 <isaac.uched@gmail.com>
2023-08-04 05:37:36 -0700

    0x14.c-Bit manipulation alx task update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,10 +7,9 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-int bit
-
+int bit_value;
 if (index > 63)
 return (-1);
-bit = (n >> index) & 1;
-return (bit);
+bit_value = (n >> index) & 1;
+return (bit_value);
 }
