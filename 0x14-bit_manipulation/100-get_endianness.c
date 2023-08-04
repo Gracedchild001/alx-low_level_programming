ee9929e28b83bd8c41d8c946a3b3b53cf975f1db
Gracedchild001 <isaac.uched@gmail.com>
2023-08-04 05:46:12 -0700

    0x14.c-Bit manipulation alx task update

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+i#include "main.h"
+/**
+*get_endianness -a funct to get endianness
+*Return: 0 if it is big, 1 if it is little endianness
+*/
+int get_endianness(void)
+{
+unsigned int s = 1;
+char *c = (char *) &i;
+return (*c);
+}
