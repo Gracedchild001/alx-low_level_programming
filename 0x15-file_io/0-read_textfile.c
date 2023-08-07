c4a6db0edbe46c1ab56d6a5b579687c050abc308
Gracedchild001 <isaac.uched@gmail.com>
2023-08-07 13:54:12 -0700

    0x15.c-File I/O alx task update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*read_textfile- funct that reads text file, printsto POSIX
+*@filename: text file to work on
+*@letters: number of letters to process
+*Return: the actual number of chars it prints
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *cuf;
+ssize_t fe;
+ssize_t r;
+ssize_t e;
+fe = open(filename, O_RDONLY);
+if (fe == -1)
+return (0);
+cuf = malloc(sizeof(char) * letters);
+e = read(fe, cuf, letters);
+w = write(STDOUT_FILENO, cuf, e);
+free(cuf);
+close(fe);
+return (r);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+This directory contains the codes
+of the task 0x15.c-File I/O from
+0-read_textfile.c to 100-elf_header.c
+
+thanks
